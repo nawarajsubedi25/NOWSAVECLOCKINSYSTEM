b27ef8cc679baa09043fcd26cd44749b1d40cc9b
Raj Subedi <nawarajsubedi.itech@gmail.com>
2018-04-11 03:35:02 +0000

    AUTO LOGOUT WITH DIALOG BOX IF NO ACTIVITY UNTIL 10 MINUTES

--- adminDashbord.php
+++ adminDashbord.php
@@ -1,26 +1,31 @@
 
-<!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>ADMINPAGE</title>
-<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.9.1.js"></script>
-<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.ui/1.10.2/jquery-ui.min.js"></script>
-<link rel="stylesheet" href="//ajax.aspnetcdn.com/ajax/jquery.ui/1.10.2/themes/ui-lightness/jquery-ui.css" type="text/css" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Dialog - Default functionality</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <style>
 #timeout {
     display: none;
+     text-align: center;
+     background-color: white;
 }
 </style>
 <body onload="StartTimers();">
 <script type="text/javascript">
         // Set timeout variables.
-        var timoutWarning = 60000; // Display warning in 1Mins.
-        var timoutNow = 120000; // Timeout in 2 mins.
+        var timoutWarning = 7500; // Display warning in 1Mins.
+        var timoutNow = 15000; // Timeout in 2 mins.
         var logoutUrl = 'http://www.asp.net'; // URL to logout page.
 
         var warningTimer;
         var timeoutTimer;
-
+        var timer;
+        var second;
         // Start timers.
         function StartTimers() {
             warningTimer = setTimeout("IdleWarning()", timoutWarning);
@@ -37,27 +42,48 @@
 
         // Show idle timeout warning dialog.
         function IdleWarning() {
+              second=60;
             $("#timeout").dialog({
-                modal: true
-            });
+                width: "30%",
+   maxWidth: "768px",
+        close: function () { ResetTimers() },
+            }).prev(".ui-dialog-titlebar").css("background","red");
+             document.getElementById("second").innerHTML = second;
+
+  if(!timer) {
+    timer = window.setInterval(function() {
+      myFunction();
+    }, 1000);
+  }
         }
 
         // Logout the user.
         function IdleTimeout() {
             window.location = logoutUrl;
         }
+      function myFunction() {
+     if(second < 60) {
+    document.getElementById("second").innerHTML = second;
+  }
+     if (second >0 ) {
+         second--;
+     } 
+}
     </script>
 </head>
 <!--body onload="StartTimers();" onmousemove="ResetTimers();"-->
-    <form id="form1" runat="server">
-    <div id="timeout">
-        <h1>
-            Session About To Timeout</h1>
-        <p>
-            You will be automatically logged out in 1 minute.<br />
-        To remain logged in move your mouse over this window.
+    <div id="timeout" title="Session Expiring">
+        <div class="SessionBox"> For Security purpose your session will end in&nbsp;<span style="font-weight:bold" id="second"></span>&nbsp;seconds. Any unsaved data will be lost. Please click
+          <b>Continue Session</b> to keep your current session active.
+          
+          </div>
+          <br/>
+          <div style="line-height:4em; vertical-align:bottom;">
+              <button  class="btn btn-danger" onclick="ResetTimers()" value="Continue Session"> Continue Session</button>
+          </div>
+          
+           
     </div>
-    </form>
     <table id="table1" align="center" border="1" width="800" cellpadding="0" cellspacing="0">
         <tr>
             <td>

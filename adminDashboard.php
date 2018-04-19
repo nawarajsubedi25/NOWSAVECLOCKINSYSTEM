2831674161dc74b01a821c44358379e68ae721dd
Raj Subedi <nawarajsubedi.itech@gmail.com>
2018-04-19 23:45:45 +0000

    Table pagination and search

--- adminDashboard.php
+++ adminDashboard.php
@@ -13,7 +13,7 @@
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
   <!------------------------------------------------------------------>
   <!--- For Auto logout ---------------------------------------------->
-<body onload="--ssStartTimers();">
+<body onload="StartTimers();">
 <script type="text/javascript">
         // Set timeout variables.
         var timoutWarning = 7500; // Display warning in 1Mins.
@@ -44,7 +44,7 @@
             $("#timeout").dialog({
                 width: "30%",
    maxWidth: "768px",
-        close: function () { ResetTimers() },
+        close: function () { ResetTimers(); },
             }).prev(".ui-dialog-titlebar").css("background","red");
              document.getElementById("second").innerHTML = second;
 
@@ -86,26 +86,28 @@
     </div>
     
          <div class="container-fluid">
+             <div class="innercontainer">
              <div class="row firstRow">
-    <div class="col-sm-3 storelogo">
+    <div class="col-sm-3 col-md-3 col-xs-3 storelogo">
     <a  href="admin.php"><img alt="logo" height="30" src="images/nowsave.png" width="139" /></a>
     </div>
-    <div class="col-sm-4 storeInformation">
+    <div class="col-sm-6 col-md-6 col-xs-6 storeInformation" align="center">
      <a href="admin.php"><i class="fas fa-home fa-2x">&nbsp;</i>DHALIWAL INC</a> |&nbsp;
      <a  href="admin.php"><i class="fas fa-laptop fa-2x">&nbsp;</i>VIJAY-VIKRAM LLC</a> |&nbsp;
       <a href="admin.php"><i class="fas fa-user fa-2x">&nbsp;</i>nowsave20</a>
     </div>
-    
-    <div class="col-sm-5 logout" align="right">
+    <div class="col-sm-3 col-md-3 col-xs-3 logout" align="right">
     <a  href="admin.php"><img alt="logo" height="30" src="images/shutdown.png" width="30" />Logout </a>
+    </div>
     </div>
     </div>
     </div>
     <div class="innercontainer">
     <div class="row SecondRow">
     </div>
-    <div class="row thirdRow">
-        <div class="col-sm-3 col-md-2 left-sidebar">
+    </div>
+    <div class="innercontainer">
+        <div class="col-sm-2 col-md-2 left-sidebar">
                 <ul class="nav nav-sidebar">
                     <li class="active"><a href="#">Overview</a></li>
                     <li><a href="#">Reports</a></li>
@@ -113,14 +115,54 @@
                     <li><a href="#">Export</a></li>
                 </ul>
             </div>
-         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+     </div>
+     <div class="right_col">
+         <div class="col-sm-12 col-md-12 col-xs-12 main">
+              <div class="col-md-6 col-sm-6">
                 <div class="selectEmployee">Find Employee
-                 <input class="inputbox" id="inputbox"  maxlength="40" autocomplete="off" name="password" type="password"  required/>
-            <input type="submit" name="formSubmit" value="Search" class="button" />
+                 <input class="inputbox" id="inputbox"  maxlength="40" autocomplete="off" name="password"  required/>
+            <input type="submit" name="formSubmit" value="Search" class="button"  onclick="searchResult()"/>
         </div>
+        </div>
+         <div class="col-md-6 col-sm-6">
+           <div id="reportrange" class="pull-right" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
+                      <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
+                      <span></span> <b class="caret"></b>
+                    </div>
+          </div>
+        </div>
+      <div class="col-sm-12 col-md-12 col-xs-12 main">
+           <div class="col-md-6 col-sm-6">
+         <div class="Filter">
+             <button id="activites" onclick="recentActivites()">Recent Activities</button>
+                    <button id="favorite" onclick="favoriteEmployee()">Favorite Employee</button>
+                    <button id="result" onclick="searchResult()">Search Result</button>
+         </div>
                  </div>
-            </div>
-    </div>
-    </div>
+                 </div>
+      <div class="col-sm-12 col-md-12 col-xs-12 main">
+           <div class="col-md-6 col-sm-6" id="recentActivites">
+         <h3>Recent Employee Activities <small>Each Employees Recent Activities</small></h3>
+         </div>
+                 </div>
+                 <div class="col-sm-12 col-md-12 col-xs-12 main">
+           <div class="col-md-6 col-sm-6" id="favoriteEmployee" style="display:none;">
+         <h3>Favorite Employee Activities <small>Each Employees Recent Activities</small></h3>
+         </div>
+                 </div>
+                 <div class="col-sm-12 col-md-12 col-xs-12 main">
+           <div class="col-md-6 col-sm-6" id="searchResult" style="display:none;">
+         <h3>Search Result <small>Each Employees Recent Activities</small></h3>
+         </div>
+                 </div>
+                 </div>
+     <!-- Include Required Prerequisites -->
+<script type="text/javascript" src="//cdn.jsdelivr.net/jquery/1/jquery.min.js"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+ 
+<!-- Include Date Range Picker -->
+<script type="text/javascript" src="js/Datepicker.js"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.js"></script>
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css" />
 </body>
 </html>
---
+++ js/Datepicker.js
@@ -0,0 +1,162 @@
+
+$(function() {
+
+    var start = moment().subtract(-1, 'days');
+    var end = moment();
+
+    function cb(start, end) {
+        $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    }
+
+    $('#reportrange').daterangepicker({
+        startDate: start,
+        endDate: end,
+        ranges: {
+           'Today': [moment(), moment()],
+           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+           'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+           'This Month': [moment().startOf('month'), moment().endOf('month')],
+           'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        }
+    }, cb);
+
+    cb(start, end);
+    
+});
+
+// Recent Activites show function
+function favoriteEmployee() {
+   document.getElementById('favoriteEmployee').style.display = "block";
+    document.getElementById('favorite').style.background = "Red";
+    document.getElementById('recentActivites').style.display = "none";
+       document.getElementById('activites').style.background = "none";
+     document.getElementById('searchResult').style.display = "none";
+        document.getElementById('result').style.background = "none";
+}
+function recentActivites() {
+   document.getElementById('recentActivites').style.display = "block";
+     document.getElementById('activites').style.background = "Red";
+    document.getElementById('searchResult').style.display = "none";
+       document.getElementById('result').style.background = "none";
+     document.getElementById('favoriteEmployee').style.display = "none";
+        document.getElementById('favorite').style.background = "none";
+}
+function searchResult() {
+   document.getElementById('searchResult').style.display = "block";
+     document.getElementById('result').style.background = "Red";
+    document.getElementById('recentActivites').style.display = "none";
+       document.getElementById('activites').style.background = "none";
+     document.getElementById('favoriteEmployee').style.display = "none";
+        document.getElementById('favorite').style.background = "none";
+}
+
+// Bootstrap pagination 
+$.fn.pageMe = function(opts){
+    var $this = this,
+        defaults = {
+            perPage: 7,
+            showPrevNext: false,
+            hidePageNumbers: false
+        },
+        settings = $.extend(defaults, opts);
+    
+    var listElement = $this;
+    var perPage = settings.perPage; 
+    var children = listElement.children();
+    var pager = $('.pager');
+    
+    if (typeof settings.childSelector!="undefined") {
+        children = listElement.find(settings.childSelector);
+    }
+    
+    if (typeof settings.pagerSelector!="undefined") {
+        pager = $(settings.pagerSelector);
+    }
+    
+    var numItems = children.size();
+    var numPages = Math.ceil(numItems/perPage);
+
+    pager.data("curr",0);
+    
+    if (settings.showPrevNext){
+        $('<li><a href="#" class="prev_link">«</a></li>').appendTo(pager);
+    }
+    
+    var curr = 0;
+    while(numPages > curr && (settings.hidePageNumbers==false)){
+        $('<li><a href="#" class="page_link">'+(curr+1)+'</a></li>').appendTo(pager);
+        curr++;
+    }
+    
+    if (settings.showPrevNext){
+        $('<li><a href="#" class="next_link">»</a></li>').appendTo(pager);
+    }
+    
+    pager.find('.page_link:first').addClass('active');
+    pager.find('.prev_link').hide();
+    if (numPages<=1) {
+        pager.find('.next_link').hide();
+    }
+  	pager.children().eq(1).addClass("active");
+    
+    children.hide();
+    children.slice(0, perPage).show();
+    
+    pager.find('li .page_link').click(function(){
+        var clickedPage = $(this).html().valueOf()-1;
+        goTo(clickedPage,perPage);
+        return false;
+    });
+    pager.find('li .prev_link').click(function(){
+        previous();
+        return false;
+    });
+    pager.find('li .next_link').click(function(){
+        next();
+        return false;
+    });
+    
+    function previous(){
+        var goToPage = parseInt(pager.data("curr")) - 1;
+        goTo(goToPage);
+    }
+     
+    function next(){
+        goToPage = parseInt(pager.data("curr")) + 1;
+        goTo(goToPage);
+    }
+    
+    function goTo(page){
+        var startAt = page * perPage,
+            endOn = startAt + perPage;
+        
+        children.css('display','none').slice(startAt, endOn).show();
+        
+        if (page>=1) {
+            pager.find('.prev_link').show();
+        }
+        else {
+            pager.find('.prev_link').hide();
+        }
+        
+        if (page<(numPages-1)) {
+            pager.find('.next_link').show();
+        }
+        else {
+            pager.find('.next_link').hide();
+        }
+        
+        pager.data("curr",page);
+      	pager.children().removeClass("active");
+        pager.children().eq(page+1).addClass("active");
+    
+    }
+};
+
+$(document).ready(function(){
+    
+  $('#myTable').pageMe({pagerSelector:'#myPager',showPrevNext:true,hidePageNumbers:false,perPage:4});
+  
+    
+});
